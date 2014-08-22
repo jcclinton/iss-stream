7b9c99936d17e4a8f1495f779841dceaa0856192
Jamie Clinton <jcclinton24@gmail.com>
2014-08-22 12:38:55 -0700

    added documentation

---
+++ doc/readme.md
@@ -0,0 +1,9 @@
+#### Location Stream Documenation
+
+###### usage
+Import lib/loc\_stream.js. Create readable stream by calling `create(id, rate, showDiff)` method. Id is the id of the satellite you wish to track, rate is the time interval between updates (measured in seconds) and showDiff is a boolean value that determines whether you will be reading the absolute lat/lng values or the relative lat/lng diffs.
+
+Id and rate must be non-negative numbers or an exception will be thrown.
+
+##### test
+To run the unit tests, run `npm-run-script test` from the command line in the top level directory of the library.
--- test/test-loc-stream.js
+++ test/test-loc-stream.js
@@ -4,11 +4,49 @@ var locStream = require('../lib/loc_stream')
 	;
 
 id = 25544;
-rate = 1; // run every second
+rate = .001; // run every second
 
 
-exports['run'] = function(test){
+exports['request'] = function(test){
 	var stream = locStream.create(id, rate, false);
-	test.equal(1, 1);
+
+	test.expect(1);
+
+	stream.on('readable', function(){
+		var buffer = iss.read()
+			, newId
+			, data
+			, lat
+			, lng
+			;
+
+		if( buffer ){
+			data = JSON.parse(buffer.toString());
+			newId = data.id;
+			lat = data.latitude;
+			lng = data.longitude;
+			test.equal(typeof lat, "number");
+			test.equal(typeof lng, "number");
+			test.equal(data.id, id);
+			stream.stop();
+		}
+		test.done();
+	});
+	stream.start();
+
+};
+
+exports['exceptions'] = function(test){
+
+	test.throws(function(){ locStream.create(0, rate, false) });
+	test.throws(function(){ locStream.create(id, 0, false) });
+	test.throws(function(){ locStream.create(-1, rate, false) });
+	test.throws(function(){ locStream.create(id, -1, false) });
+
+	test.throws(function(){ locStream.create('a', rate, false) });
+	test.throws(function(){ locStream.create(id, 'a', false) });
+	test.throws(function(){ locStream.create(null, rate, false) });
+	test.throws(function(){ locStream.create(id, null, false) });
+
 	test.done();
 };
