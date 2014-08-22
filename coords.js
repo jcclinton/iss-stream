3d207667dd31405295b0ec3303d73d08200ed390
Jamie Clinton <jcclinton24@gmail.com>
2014-08-21 21:55:15 -0700

    more code cleanup

--- coords.js
+++ coords.js
@@ -4,48 +4,77 @@ var https = require('https');
 var _ = require('underscore');
 
 
+exports.create = function(id, rate, transform){
+	return new LocStream(id, rate, transform);
+};
+
+
 // dont reject self signed ca
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 
-util.inherits(Coords, Readable);
 
-function Coords(id, rate, transform){
+util.inherits(LocStream, Readable);
+
+
+// constructor for location stream
+// id is the id of the satellite you want to track
+// rate is how often you want to request info, it is measured in seconds between requests
+// showDiff is a boolean that determines whether the lat/long relative to the last update will be shown or the absolute lat/long values
+function LocStream(id, rate, showDiff){
 	if(!_.isNumber(id) || id <= 0){
 		throw "invalid id";
 	}
 
 	// rate is the number of requests per minute
 	// it must be a non-negative integer
-	if( !_.isNumber(rate) || rate <= 1 ){
+	if( !_.isNumber(rate) || rate <= 0 ){
 		throw "invalid rate";
 	}
 
 	Readable.call(this);
 
 	this._url = "https://api.wheretheiss.at/v1/satellites/" + id;
-	this._timeout = Math.round((60 * 1000) / rate);
+	this._timeout = rate * 1000;
 	this._running = false;
-	this._transform = transform;
+	this._showDiff = showDiff;
 	this.lastLat = null;
 	this.lastLng = null;
+	this._delayTimer = null;
 }
 
-Coords.prototype._read = function(n){
+LocStream.prototype._read = function(n){
 	return this.push('');
 }
 
-Coords.prototype.start = function(){
+// starts requesting data from the server
+LocStream.prototype.start = function(){
 		if( this._running ){
 			return;
 		}
+		// clean up, just in case
+		clearTimeout(this._delayTimer);
+
 		this._running = true;
-		this._run();
+		run.call(this);
 };
 
 
-Coords.prototype._run = function(){
+// stops requests and stops putting data in the stream
+LocStream.prototype.stop = function(){
+	// stop timeout if one exists
+	clearTimeout(this._delayTimer);
+	this._running = false;
+}
+
+
+
+// private functions
+
+function run(){
 		var that = this;
 
+	// if we are not running, dont do anything
+	// if there is already a main timer, dont do anything
 	if( !this._running ){
 		return;
 	}
@@ -54,26 +83,29 @@ Coords.prototype._run = function(){
 
 		//console.log(res.headers["x-rate-limit-remaining"]);
 
-		// wheretheiss returns a 429 status code when the timeout has been exceeded
 		if( res.statusCode == 200 ){
 			res.on('data', function(data){
-				that.push(that._format(data));
+				that.push(format.call(that, data));
 			});
 
 			// set next request to fire after a timeout
-			setTimeout(function(){
-				that._run();
-			}, that._timeout);
+			if( that._running ){
+				setTimeout(function(){
+					run.call(that);
+				}, that._timeout);
+			}
 
 		}else if( res.statusCode == 404 ){
+			// no data found for given id
 			that.stop();
 			that.emit('error');
 		}else{
+			// wheretheiss returns a 429 status code when the timeout has been exceeded
 			// if we hit the rate limit, wait 60 seconds to start again
 			var t = 1000 * 60;
 
 			that.stop();
-			setTimeout(function(){
+			this._delayTimer = setTimeout(function(){
 				that.start();
 			}, t);
 		}
@@ -82,52 +114,53 @@ Coords.prototype._run = function(){
 
 }
 
-
-Coords.prototype.stop = function(){
-	this._running = false;
-}
-
-Coords.prototype._format = function(buffer){
+// takes the given json object from the server
+// gives it proper formatting
+// output of this function gets pushed to the stream
+function format(buffer){
 
 	var lat
 		, lng
-		, diff
+		, output
 		, data
 		;
 
-	// if we are not transforming the data
-	// just pass it straight through
-	if( !this._transform ){
-		return buffer;
-	}
-
-	//data = JSON.parse(buffer.toJSON());
+	// convert buffer to object
 	data = JSON.parse(buffer.toString());
 
 	lat = data.latitude;
 	lng = data.longitude;
-	//console.log(data);
-	//console.log(this.lastLat);
 
 
-	if( this.lastLat == null || this.lastLng == null ){
+	// start building up the object that will be pushed to the stream
+	output = {
+		"id": data.id
+	};
+
+
+	// if we only care about lat/long difference
+	if( this._showDiff ){
+
+		// first time running
+		// dont push data onto stream
+		// since there is nothing to show
+		if( this.lastLat == null || this.lastLng == null ){
+			this.lastLat = lat;
+			this.lastLng = lng;
+			return '';
+		}
+
+
+		output.latDiff = this.lastLat - lat;
+		output.lngDiff = this.lastLng - lng;
 		this.lastLat = lat;
 		this.lastLng = lng;
-		return '';
+
+	}else{
+		// print absolute lat/long values
+		output.latitude = lat;
+		output.longitude = lng;
 	}
 
-	diff = {};
-
-	diff.latDiff = this.lastLat - lat;
-	diff.lngDiff = this.lastLng - lng;
-	this.lastLat = lat;
-	this.lastLng = lng;
-
-	return new Buffer(JSON.stringify(diff));
+	return new Buffer(JSON.stringify(output));
 }
-
-
-
-exports.create = function(id, rate, transform){
-	return new Coords(id, rate, transform);
-};
--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var coords = require("./coords.js");
 
 var id = 25544;
-var rate = 60; // 60 requests / second
+var rate = 2; // run every 2 seconds
 
 // create coords stream for iss
 var iss = coords.create(id, rate, true);
@@ -9,7 +9,7 @@ var iss = coords.create(id, rate, true);
 iss.on('readable', function(){
 	var data = iss.read();
 	if( data ){
-		console.log('data readeable: ' + data);
+		console.log(""+data);
 	}
 });
 
@@ -24,3 +24,13 @@ iss.on('error', function(){
 });
 
 iss.start();
+
+setTimeout(function(){
+	console.log("stopping");
+	iss.stop();
+}, 6000);
+
+setTimeout(function(){
+	console.log("starting");
+	iss.start();
+}, 15000);
